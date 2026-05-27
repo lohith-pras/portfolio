ee3a0ffb2ae54142631b2527fa6faaeabd643088
lohith-pras <loh.t.prasanna@fau.de>
2026-05-28 00:32:03 +0200

    feat: add RPM avatar URL constant

---
+++ src/lib/avatar.ts
@@ -0,0 +1,3 @@
+// Replace YOUR_AVATAR_ID with your Ready Player Me avatar ID from readyplayer.me/hub
+export const AVATAR_URL =
+  'https://models.readyplayer.me/YOUR_AVATAR_ID.glb?quality=medium&textureSizeLimit=512'
